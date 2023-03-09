e69a2338610b646215b2cc562d36d2d26dd1094b
QuyetTran <quyettk@zen-s.com>
2023-03-09 17:01:58 +0700

    hot-fix: add link android app

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <div className="flex flex-col justify-center px-[26px] pt-[43.98px] pb-[60.07px] w-full h-full">
+      <div>Footer</div>
+    </div>
+  );
+};
